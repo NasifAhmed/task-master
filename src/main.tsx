15cad2b9174bc49f20672b81de97e8ce6d0e37e8
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-12-22 01:43:03 +0600

    Add authprovider to main

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
+import AuthProvider from "./provider/AuthProvider.tsx";
 import { router } from "./router/router.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>
+    <AuthProvider>
+        <React.StrictMode>
+            <RouterProvider router={router} />
+        </React.StrictMode>
+    </AuthProvider>
 );
