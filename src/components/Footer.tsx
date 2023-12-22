5cbfc3a2c625e54462478c95f6ebe7f7afffc1a0
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-12-22 12:52:51 +0600

    Structure footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,14 @@
+import Logo from "./Logo";
+
 export default function Footer() {
-    return <div>Footer</div>;
+    return (
+        <div className="mt-10 py-20 flex items-center justify-around bg-muted">
+            <div className="leftside flex flex-col justify-center items-center">
+                <Logo />
+            </div>
+            <div className="rightside flex flex-col justify-center items-center">
+                right
+            </div>
+        </div>
+    );
 }
