9a7fde4de9d70dabeed6dc339eab1e8d2d39186e
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-12-22 15:40:12 +0600

    Setup footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,36 @@
+import { Facebook, Twitter, Youtube } from "lucide-react";
 import Logo from "./Logo";
 
 export default function Footer() {
     return (
-        <div className="mt-10 py-20 flex items-center justify-around bg-muted">
-            <div className="leftside flex flex-col justify-center items-center">
-                <Logo />
+        <footer className="flex flex-col bg-muted">
+            <div className="mt-10 py-20 flex items-center justify-around ">
+                <div className="leftside flex flex-col justify-center items-center">
+                    <Logo />
+                    <h2 className="font-semibold text-lg mt-3">Task Master</h2>
+                </div>
+                <div className="rightside flex gap-5 justify-center items-center">
+                    <a href="http://www.facebook.com">
+                        <Facebook />
+                    </a>
+                    <a href="http://www.x.com">
+                        <Twitter />
+                    </a>
+                    <a href="http://www.youtube.com">
+                        <Youtube />
+                    </a>
+                </div>
             </div>
-            <div className="rightside flex flex-col justify-center items-center">
-                right
+            <div className="mb-5 px-4 text-center text-slate-500">
+                <small className="mb-2 text-xs block">
+                    &copy; 2023 Nasif Ahmed. All rights reserved.
+                </small>
+
+                <p className="text-xs">
+                    <span className="font-bold">About this website: </span>{" "}
+                    built with React + Typescript, Tailwind CSS{" "}
+                </p>
             </div>
-        </div>
+        </footer>
     );
 }
