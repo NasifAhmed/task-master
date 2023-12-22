8b8e3cef54ce23251e3344dd1c73b149ba7fe677
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-12-22 15:40:24 +0600

    Add all the banners

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,13 @@
+import Banner from "@/components/Banner";
+import DeveloperBanner from "@/components/DeveloperBanner";
+import StudentBanner from "@/components/StudentBanner";
+
 export default function Home() {
-    return <div>Home</div>;
+    return (
+        <div className="md:space-y-40 space-y-20">
+            <Banner />
+            <DeveloperBanner />
+            <StudentBanner />
+        </div>
+    );
 }
